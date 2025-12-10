54993f54c3a6a8a4172ef457d7eabbc98de289f0
olir27 <jolir27@gmail.com.com>
2025-12-10 09:33:36 +0530

    change hosted URL 04

--- src/server.js
+++ src/server.js
@@ -93,7 +93,8 @@ const PORT = process.env.PORT || 5000;
 
 // -------------------- MIDDLEWARE --------------------
 app.use(cors({
-  origin: ["http://localhost:5173","https://veg-pack-frontend.vercel.app"],
+  // origin: ["http://localhost:5173","https://veg-pack-frontend.vercel.app"],
+  origin:"https://veg-pack-frontend.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
